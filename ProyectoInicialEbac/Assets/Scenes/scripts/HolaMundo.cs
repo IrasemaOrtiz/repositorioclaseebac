fed7c9010e91ba1f0e61ff2d109383510cbf4e92
Irasema Ortiz <chemisira29@gmail.com>
2022-04-13 19:57:59 -0500

    se hicieron las impresiones de consola
    se hicieron las impresiones vistas en clase usando debug en las funciones varias desde awake a late update

--- ProyectoInicialEbac/Assets/Scenes/scripts/HolaMundo.cs
+++ ProyectoInicialEbac/Assets/Scenes/scripts/HolaMundo.cs
@@ -13,6 +13,22 @@ public class HolaMundo : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      
+    }
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("hola desde Fixed Update cada 50 frames");
+    }
+    private void LateUpdate()
+    {
+        Debug.Log("hola desde Late Update");
+    }
+    private void OnEnable()
+    {
+        Debug.LogWarning("el objeto ha sido habilitado");
+    }
+    private void OnDisable()
+    {
+        Debug.LogWarning("el objeto ha sido inhabilitado"); 
     }
 }
